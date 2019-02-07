1398a207928b48c011339071759aabfc8d6f876d
Tonyjlum <tonyjlum@gmail.com>
2019-02-07 16:11:36 -0500

    start seed data, 3 chefs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Chef.create(name:"Walter White", specialty: "mexican", price: 40, rating: 5, profile_picture_url:"https://media1.s-nbcnews.com/j/streams/2012/October/121010/1C4235316-121010-ent-cranston-hmed.fit-760w.jpg")
+
+Chef.create(name:"Boyardee", specialty:"italian", price: 22, rating: 2, profile_picture_url:"https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Chefboyardeepic.jpg/220px-Chefboyardeepic.jpg")
+
+Chef.create(name:"Gordon Ramsay", specialty:"french", price: 83, rating: 4, profile_picture_url:"https://media.brstatic.com/2017/04/24173442/gordan-ramsay-chef-cooks-ribs-mst.jpg")
