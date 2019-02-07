119b2cdc3622a231e59f19b40b513956c3b110ec
Chris Benedict <chrisjbenedict@gmail.com>
2019-02-07 15:08:48 -0500

    migration

--- db/migrate/20190207185811_create_chefs.rb
+++ db/migrate/20190207185811_create_chefs.rb
@@ -1,11 +1,11 @@
 class CreateChefs < ActiveRecord::Migration[5.2]
   def change
-    create_table :api_v1_chefs do |t|
+    create_table :chefs do |t|
       t.string :name
       t.string :specialty
       t.integer :price
       t.integer :rating
-      t.string: :profile_picture_url
+      t.string :profile_picture_url
 
       t.timestamps
     end
