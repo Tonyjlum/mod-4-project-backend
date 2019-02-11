e72fef03c3573ef42e424a59a0946d1d7c3d5dfe
Chris Benedict <chrisjbenedict@gmail.com>
2019-02-11 10:19:19 -0500

    controllers fix

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -5,4 +5,31 @@ class Api::V1::AppointmentsController < ApplicationController
     render json: @appointments
   end
 
+  def show
+    @appointment = Appointment.find(params[:id])
+    render json: @appointment, status: :ok
+  end
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+    render json: @appointment, status: :created
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    @appointment.update(appointment_params)
+    render json: @appointment, status: :accepted
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    @appointment.delete
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:user_id, :guest_count, :chef_id, :cost, :note, :datetime)
+  end
+
 end
--- app/serializers/api/v1/appointment_serializer.rb
+++ app/serializers/api/v1/appointment_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::AppointmentSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :chef_id, :guest_count, :cost, :speical_note, :datetime
+  attributes :id, :user_id, :chef_id, :guest_count, :cost, :note, :datetime
 end
